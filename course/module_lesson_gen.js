1a81a12bc92bf59efc9051c2b8793e6ac534449f
Abdullah Mustapha <abdullahmlsu@gmail.com>
2023-01-17 17:03:37 +0100

    Add: back to course button on each module page.

--- course/module_lesson_gen.js
+++ course/module_lesson_gen.js
@@ -28,6 +28,11 @@ let parameter2 = paramString[1].split("=")[1]
   `;
   }
 
+  function courseSectionUI(name){
+    return `<a href="course.html?course=${name.replaceAll(/\s/g, '')}"><h2 style="padding: 5%;"> <i class="fa fa-arrow-left" aria-hidden="true"></i>
+    ${name} </h2> </a>`
+  }
+
 
 
 
@@ -37,6 +42,7 @@ let parameter2 = paramString[1].split("=")[1]
   //Set Courses
   for (let i = 0; i < data.courses.length; i++) {
     if(data.courses[i].name.replaceAll(/\s/g, '')==parameter1){
+      document.getElementById('course-title').insertAdjacentHTML('afterbegin', courseSectionUI(data.courses[i].name)) //Course Name
         
         for (let j = 0; j < data.courses[i].modules.length; j++) {
             if(data.courses[i].modules[j].name.replaceAll(/\s/g, '')==parameter2){
